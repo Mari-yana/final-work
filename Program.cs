8e2885194b02606cc6fe43cc96b4c18d7bdd9fea
Mariana <mariana.panasenko@gmail.com>
2022-09-17 13:49:50 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -3,12 +3,19 @@ Console.Write ("Enter array elements separated by spaces: ");
 string inputString = Console.ReadLine();
 var array1 = inputString.Split(' ').ToArray();
 
-var list = new List<string>();
-foreach (var element in array1) {
-     if (element.Length <= 3) list.Add(element); 
+int count = 0; 
+foreach (var element in array1) { 
+    if (element.Length <= 3) count++; 
 } 
-var array2 = new string[list.Count]; 
-list.CopyTo(array2);
+
+var array2 = new string[count]; 
+int j = 0; 
+
+for (int i = 0; i < array1.Length; i++) { 
+    if (array1[i].Length <= 3) {
+       array2[j++] = array1[i]; 
+    }
+}
 
 var result = string.Join(" ", array2);
 Console.WriteLine(result);
